edf9050b517b11625adae957d3a162456c669545
neajul <info@lukasengelhardt.net>
2021-10-19 14:06:22 +0200

    modular image zoom system

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
+// hard vars
+numberOfImages = 4;
+
 // adjustable variables
 const minTime = 500;
-const targetPointMargin = 20;
+const targetPointMargin = 5;
 
 // get window width
 const windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
@@ -53,72 +56,94 @@ loader.load((loader, resources) => {
 loader.onComplete.add(() => {
   // console.log("loaded");
 
+  // dynamically create some images, and their counterparts
+  masked = [];
+  mask = [];
+  alpha = [];
+  targetPoints = [];
+  elapsed = [];
 
-  // define the image that will be masked
-  const masked1 = new PIXI.Sprite();
-  masked1.time = minTime + minTime * Math.random();
-  masked1.alpha = 0;
-  addRandomImage(masked1, sprites);
-  placeImage(masked1);
+  for (var i = 0; i < numberOfImages; i++) {
+    // define the image that will be masked
+    masked[i] = new PIXI.Sprite();
+    masked[i].time = minTime + minTime * Math.random();
+    masked[i].alpha = 0;
+    addRandomImage(masked[i], sprites);
+    placeImage(masked[i]);
 
-  // create mask for this image
-  const mask1 = new PIXI.Sprite();
-  mask1.texture = masked1.texture;
-  placeImage(mask1);
-  // define the mask image as the mask for the top image
-  masked1.mask = mask1;
+    // create mask for this image
+    mask[i] = new PIXI.Sprite();
+    mask[i].texture = masked[i].texture;
+    placeImage(mask[i]);
+    // define the mask image as the mask for the top image
+    masked[i].mask = mask[i];
 
-  // create alpha for this image
-  const alpha1 = new PIXI.Sprite();
-  alpha1.texture = masked1.texture;
-  alpha1.alpha = 0;
-  placeImage(alpha1);
+    // create alpha for this image
+    alpha[i] = new PIXI.Sprite();
+    alpha[i].texture = masked[i].texture;
+    alpha[i].alpha = 0;
+    placeImage(alpha[i]);
+
+    // create targetpoints for this image
+    targetPoints[i] = new PIXI.Point();
+
+    // create new counter
+    elapsed[i] = 0.0;
+  }
 
   // create a point where the mask image will move towards
-  const targetPoint1 = new PIXI.Point();
   resetTargetPoints();
 
   // reset basically moves this point randomly, and the mask will then start moving there
   function resetTargetPoints() {
-      targetPoint1.x = ( ( Math.random() - .5 ) * targetPointMargin ) + ( windowWidth / 2 );
-      targetPoint1.y = ( ( Math.random() - .5 ) * targetPointMargin ) + ( windowHeight / 2 );
+    for (var i = 0; i < targetPoints.length; i++) {
+      targetPoints[i].x  = ( ( Math.random() - .5 ) * targetPointMargin ) + ( windowWidth / 2 );
+      targetPoints[i].y  = ( ( Math.random() - .5 ) * targetPointMargin ) + ( windowWidth / 2 );
+    }
   }
 
-  // Add a variable to count up the seconds our demo has been running
-  let elapsed = 0.0;
+
 
   // Tell our application's ticker to run a new callback every frame, passing
   // in the amount of time that has passed since the last tick
   app.ticker.add((delta) => {
-    // Add the time to our total elapsed time
-    elapsed += delta;
-
-    // move the mask towards the targetpoint
-    mask1.x += (targetPoint1.x - mask1.x) * 0.03;
-    mask1.y += (targetPoint1.y - mask1.y) * 0.03;
-
-    // scale images universally
-    masked1.scale.set( masked1.scale._x * 1.001 );
-    mask1.scale.set( mask1.scale._x * 1.0011 );
-    alpha1.scale.set( alpha1.scale._x * 1.001 );
-    masked1.alpha = masked1.alpha + 0.001;
-    alpha1.alpha = alpha1.alpha + 0.0005;
-
-    // every couple frames, reset everything
-    // if (elapsed > masked1.time) {
-    //   addRandomImage(masked1, sprites);
-    //   placeImage(masked1);
-    //   addRandomImage(mask1, sprites);
-    //   placeImage(mask1);
-    //   elapsed = 0;
-    //   // reset the point that the mask moves towards
-    //   resetTargetPoints();
-    // }
+    // for each image instance do a thing
+    for (var i = 0; i < numberOfImages; i++) {
+      const maxTime = masked[i].time;
+      // count time
+      elapsed[i] += delta;
+      // turn up alpha
+      masked[i].alpha = masked[i].alpha + 1 / masked[i].time / 2;
+      alpha[i].alpha = alpha[i].alpha + 1 / masked[i].time;
+      // move mask towards target point
+      // mask[i].x += (targetPoints[i].x - mask[i].x) * 1 / masked[i].time;
+      // mask[i].y += (targetPoints[i].y - mask[i].y) * 1 / masked[i].time;
+      // scale image
+      masked[i].scale.set( masked[i].scale._x * 1.001 );
+      mask[i].scale.set( mask[i].scale._x * 1.002 );
+      alpha[i].scale.set( alpha[i].scale._x * 1.001 );
+      // if time is up, reset everything
+      if (elapsed[i] > maxTime) {
+        // make a new base image
+        masked[i].alpha = 0;
+        addRandomImage(masked[i], sprites);
+        placeImage(masked[i]);
+        // new mask
+        mask[i].texture = masked[0].texture;
+        placeImage(mask[i]);
+        // new alpha
+        alpha[i].texture = masked[i].texture;
+        placeImage(alpha[i]);
+        alpha[i].alpha = 0;
+        // reset timer
+        elapsed[i] = 0;
+        // reset the point that the mask moves towards
+        resetTargetPoints();
+      }
+    }
   });
 
 }); // called once when the queued resources all load.
-
-
 
 
 
