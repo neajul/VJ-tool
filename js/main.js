c3a2c78db330d4dc2a590bd221d6e36ba456e389
neajul <info@lukasengelhardt.net>
2021-10-19 12:33:06 +0200

    using jonathan's images

--- js/main.js
+++ js/main.js
@@ -29,9 +29,18 @@ document.body.appendChild(app.view);
 // load all image resources
 const loader = PIXI.Loader.shared;
 const sprites = {};
-loader.add('one', 'img/2004273_PS5.jpg')
-      .add('two', 'img/test.jpg')
-      .add('three', 'img/arseniy-chebynkin-sermetch.jpg');
+loader.add('img1', 'img/IMG_2110.JPG')
+      .add('img2', 'img/IMG_2115.JPG')
+      .add('img3', 'img/IMG_2116.JPG')
+      .add('img4', 'img/IMG_2121.JPG')
+      .add('img5', 'img/IMG_2122.JPG')
+      .add('img6', 'img/IMG_4496.JPG')
+      .add('img7', 'img/IMG_4592.JPG')
+      .add('img8', 'img/IMG_4804.JPG')
+      .add('img9', 'img/IMG_4854.JPG')
+      .add('img10', 'img/IMG_4860.JPG')
+      .add('img11', 'img/IMG_4680.JPG')
+      .add('img12', 'img/arseniy-chebynkin-sermetch.jpg');
 
 // The `load` method loads the queue of resources, and calls the passed in callback called once all
 // resources have loaded.
@@ -42,9 +51,18 @@ loader.load((loader, resources) => {
     // - `error`: The error that happened when trying to load (if any)
     // - `data`: The raw data that was loaded
     // also may contain other properties based on the middleware that runs.
-    sprites.one = new PIXI.Sprite(resources.one.texture);
-    sprites.two = new PIXI.Sprite(resources.two.texture);
-    sprites.three = new PIXI.Sprite(resources.three.texture);
+    sprites.img1 = new PIXI.Sprite(resources.img1.texture);
+    sprites.img2 = new PIXI.Sprite(resources.img2.texture);
+    sprites.img3 = new PIXI.Sprite(resources.img3.texture);
+    sprites.img4 = new PIXI.Sprite(resources.img4.texture);
+    sprites.img5 = new PIXI.Sprite(resources.img5.texture);
+    sprites.img6 = new PIXI.Sprite(resources.img6.texture);
+    sprites.img7 = new PIXI.Sprite(resources.img7.texture);
+    sprites.img8 = new PIXI.Sprite(resources.img8.texture);
+    sprites.img9 = new PIXI.Sprite(resources.img9.texture);
+    sprites.img10 = new PIXI.Sprite(resources.img10.texture);
+    sprites.img11 = new PIXI.Sprite(resources.img11.texture);
+    sprites.img12 = new PIXI.Sprite(resources.img12.texture);
 });
 
 // when all images are loaded we can start
@@ -52,18 +70,18 @@ loader.onComplete.add(() => {
   // console.log("loaded");
 
   // define background image
-  const bg = sprites.one;
+  const bg = sprites.img1;
   // place random image
   addRandomImage(bg, sprites);
   placeImage(bg);
 
   // define the image that will be masked
-  const top = sprites.two;
+  const top = sprites.img2;
   addRandomImage(top, sprites);
   placeImage(top);
 
   // define the image that masks the top image
-  const mask = sprites.three;
+  const mask = sprites.img3;
   addRandomImage(mask, sprites);
   placeImage(mask);
 
@@ -81,40 +99,39 @@ loader.onComplete.add(() => {
       targetPoint.y = ( ( Math.random() - .5 ) * targetPointMargin ) + ( windowHeight / 2 );
   }
 
-
-
-
-
-
-
-
-
   // Add a variable to count up the seconds our demo has been running
   let elapsed = 0.0;
+
   // Tell our application's ticker to run a new callback every frame, passing
   // in the amount of time that has passed since the last tick
   app.ticker.add((delta) => {
     // Add the time to our total elapsed time
     elapsed += delta;
 
-    // move the mask towards the targetpoint
-    mask.x += (targetPoint.x - mask.x) * 0.1;
-    mask.y += (targetPoint.y - mask.y) * 0.1;
-
-    // if the mask has reached it's target, move the target
-    if (Math.abs(mask.x - targetPoint.x) < 1) {
-        reset();
-    }
+    // // move the mask towards the targetpoint
+    // mask.x += (targetPoint.x - mask.x) * 0.1;
+    // mask.y += (targetPoint.y - mask.y) * 0.1;
+    //
+    // // if the mask has reached it's target, move the target
+    // if (Math.abs(mask.x - targetPoint.x) < 1) {
+    //     reset();
+    // }
 
     // scale mask, bg, top img
     bg.scale.set( bg.scale._x * 1.001 );
     mask.scale.set( mask.scale._x * 1.001 );
     top.scale.set( top.scale._x * 1.001 );
 
-    // if (bg.scale._x > 1) {
-    //   addRandomImage(bg, sprites);
-    //   placeImage(bg);
-    // }
+    // every couple frames, reset everything
+    if (elapsed > 100) {
+      addRandomImage(bg, sprites);
+      placeImage(bg);
+      addRandomImage(mask, sprites);
+      placeImage(mask);
+      addRandomImage(top, sprites);
+      placeImage(top);
+      elapsed = 0;
+    }
   });
 
 }); // called once when the queued resources all load.
